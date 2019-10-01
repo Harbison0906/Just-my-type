90c0a3e587660f4e10bc0fc6ba54e4bdd0f72c69
Seth Harbison <sethharbison@Seths-MacBook-Pro.local>
2019-09-30 21:18:42 -0500

    added .keypress and .keyup events to highlight keys when they are pressed

--- scripts.js
+++ scripts.js
@@ -1,20 +1,36 @@
+// hide upper keyboard when page loads
 
 let upperKeys = $('#keyboard-upper-container');
 let lowerKeys = $('#keyboard-lower-container');
 $(upperKeys).hide();
 
-$(document).keydown(function(e) {
+// only show upper keys when Shift is held down and
+// only show lower keys when not holding Shift
+
+$('body').keydown(function(e) {
   if (e.keyCode == 16) {
     $(upperKeys).show();
     $(lowerKeys).hide();
   };
 });
 
-$(document).keyup(function(e) {
+$('body').keyup(function(e) {
   if (e.keyCode == 16) {
     $(upperKeys).hide();
     $(lowerKeys).show();
   };
 });
+
+// highlight a key when it is pressed
+
+$('body').keypress(function(e) {
+  $('#' + e.charCode).addClass('highlight');
+});
+
+$('body').keyup(function(e) {
+  $('.highlight').removeClass('highlight');
+})
+
+  
 
 
